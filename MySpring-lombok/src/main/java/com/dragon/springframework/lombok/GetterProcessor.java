206d6047a104c934d600680108a95620bd064c98
SuccessZhang <zhangzicheng@weidian.com>
2020-07-26 08:24:24 +0800

    完善注释

--- MySpring-lombok/src/main/java/com/dragon/springframework/lombok/GetterProcessor.java
+++ MySpring-lombok/src/main/java/com/dragon/springframework/lombok/GetterProcessor.java
@@ -30,9 +30,24 @@ import java.util.Set;
  */
 public class GetterProcessor extends AbstractProcessor {
 
+    /**
+     * 用于在编译期打log。
+     */
     private Messager messager;
+
+    /**
+     * 待处理的抽象语法树。
+     */
     private JavacTrees trees;
+
+    /**
+     * 用于创建抽象语法树的节点。
+     */
     private TreeMaker treeMaker;
+
+    /**
+     * 用于创建标识符。
+     */
     private Names names;
 
     @Override
@@ -45,6 +60,9 @@ public class GetterProcessor extends AbstractProcessor {
         this.names = Names.instance(context);
     }
 
+    /**
+     * 支持的注解类型。
+     */
     @Override
     public Set<String> getSupportedAnnotationTypes() {
         Set<String> stringSet = new LinkedHashSet<>();
@@ -52,6 +70,9 @@ public class GetterProcessor extends AbstractProcessor {
         return stringSet;
     }
 
+    /**
+     * 最低支持的JDK版本。
+     */
     @Override
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.RELEASE_8;
@@ -74,7 +95,7 @@ public class GetterProcessor extends AbstractProcessor {
                     }
                     jcVariableDeclList.forEach(jcVariableDecl -> {
                         messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
-                        jcClassDecl.defs = jcClassDecl.defs.prepend(makeGetterMethodDecl(jcVariableDecl));
+                        jcClassDecl.defs = jcClassDecl.defs.prepend(defineGetterMethod(jcVariableDecl));
                     });
                     super.visitClassDef(jcClassDecl);
                 }
@@ -84,13 +105,19 @@ public class GetterProcessor extends AbstractProcessor {
         return true;
     }
 
-    private JCTree.JCMethodDecl makeGetterMethodDecl(JCTree.JCVariableDecl jcVariableDecl) {
+    /**
+     * 在AST语法树中定义Getter方法。
+     */
+    private JCTree.JCMethodDecl defineGetterMethod(JCTree.JCVariableDecl jcVariableDecl) {
         ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
         statements.append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names.fromString("this")), jcVariableDecl.getName())));
         JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
         return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), getNewMethodName(jcVariableDecl.getName()), jcVariableDecl.vartype, List.nil(), List.nil(), List.nil(), body, null);
     }
 
+    /**
+     * 生成getXxxx方法名。
+     */
     private Name getNewMethodName(Name name) {
         String s = name.toString();
         return names.fromString("get" + s.substring(0, 1).toUpperCase() + s.substring(1, name.length()));
