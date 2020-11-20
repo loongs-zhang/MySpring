49148cf45370bd4fb79ae89218af14ab4b7e6e46
SuccessZhang <zhangzicheng@weidian.com>
2020-11-20 21:07:22 +0800

    完善注释

--- MySpring-design/src/main/java/com/example/design/observer/after/Observer.java
+++ MySpring-design/src/main/java/com/example/design/observer/after/Observer.java
@@ -3,9 +3,15 @@ package com.example.design.observer.after;
 import com.example.design.observer.before.State;
 
 /**
+ * 观察者
+ *
  * @author SuccessZhang
  * @date 2020/06/01
  */
 public interface Observer {
+
+    /**
+     * 响应老师状态的变化
+     */
     void reply(State state);
 }
